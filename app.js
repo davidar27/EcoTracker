c1ca4f0b33e41fd4b35e8aca3287ed5ccc01dda1
Martiinhz <once11caldas.martin@gmail.com>
2024-11-23 15:20:19 -0500

    Estructura del menu

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 function iniciarAplicacion() {
     alert("Bienvenido a EcoTracker: Aprende sobre el cambio climático y cómo reducir tu impacto en el medio ambiente.");
 }
+
+let Menu= true;
+
+while (Menu) {
+    const opciones = prompt(
+      "Escoja un tema escribiendo el numero correspondiente:\n" +
+        "1. Introducciin al Cambio Climatico\n" +
+        "2. Huella de Carbono Personal\n" +
+        "3. Consejos Practicos para Reducir tu Impacto\n" +
+        "4. Salir"
+    );
+
+    switch (opciones) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break
+        default:
+            break;
+    }
+}
+
