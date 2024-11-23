8470dd9810803a7f3705ea0f70ddc8261f35e2ab
davidar27 <jhonatanarcos10@gmail.com>
2024-11-23 16:10:02 -0500

    2 modulos nuevos

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
 function iniciarAplicacion() {
     alert("Bienvenido a EcoTracker: Aprende sobre el cambio climático y cómo reducir tu impacto en el medio ambiente.");
-}
 
-let Menu = true;
+    let Menu = true;
 
-while (Menu) {
-    const opciones = prompt(
-        "Escoja un tema escribiendo el número correspondiente:\n" +
-        "1. Introducción al Cambio Climático\n" +
-        "2. Huella de Carbono Personal\n" +
-        "3. Consejos Prácticos para Reducir tu Impacto\n" +
-        "4. Salir"
-    );
+    while (Menu) {
+        const opciones = prompt(
+            "Escoja un tema escribiendo el número correspondiente:\n" +
+            "1. Introducción al Cambio Climático\n" +
+            "2. Huella de Carbono Personal\n" +
+            "3. Consejos Prácticos para Reducir tu Impacto\n" +
+            "4. Salir"
+        );
 
-    switch (opciones) {
-        case 1:
-            moduloIntroduccion();
-            break;
-        case 2:
-            moduloCalculadoraHuella();
-            break;
-        case 3:
-            moduloConsejos();
-            break
-        case 4:
-            alert("Gracias por usar EcoTracker. ¡Hasta luego!");
-            Menu = false;
-            break;
-        default:
-            alert("Opción no válida. Por favor, selecciona un número del 1 al 4.");
-            break;
+        switch (opciones) {
+            case "1":
+                moduloIntroduccion();
+                break;
+            case "2":
+                moduloCalculadoraHuella();
+                break;
+            case "3":
+                moduloConsejos();
+                break
+            case "4":
+                alert("Gracias por usar EcoTracker. ¡Hasta luego!");
+                Menu = false;
+                break;
+            default:
+                alert("Opción no válida. Por favor, selecciona un número del 1 al 4.");
+                break;
+        }
     }
 }
+
+
 
 function moduloIntroduccion() {
     alert(
@@ -43,3 +45,55 @@ function moduloIntroduccion() {
         "- Desperdicio y residuos: La descomposición de residuos orgánicos en vertederos genera metano."
     );
 }
+
+
+function moduloCalculadoraHuella() {
+
+    const transporte = prompt("¿Con qué frecuencia usas transporte privado? (Responde: diario, semanal, nunca)");
+    const carne = confirm("¿Consumes carne roja más de 3 veces por semana? Responde: (Aceptar= si/ Cancelar= no)");
+
+    let huella = 0;
+
+    if (transporte === "diario") {
+        huella += 5;
+    } else if (transporte === "semanal") {
+        huella += 2;
+    } else if (transporte === "nunca") {
+        huella += 0.5;
+    } else {
+        alert("Respuesta no válida para transporte. Asignando valor por defecto: 1 tonelada.");
+        huellaCarbono += 1;
+    }
+
+
+    if (carne) {
+        huella += 2;
+    } else {
+        huella += 0.5;
+    }
+
+
+    alert(`Tu huella de carbono estimada es de ${huella.toFixed()} toneladas de CO₂ al año.\nLa media global es de 4 toneladas.`)
+
+    return huella
+}
+function moduloConsejos(transporte, carne) {
+
+    transporte = "diario"
+    carne = true
+
+    if ((transporte === "diario" || transporte === "semanal") && carne) {
+        alert(
+            "- Optar por transporte público o bicicleta puede reducir tu impacto ambiental significativamente.\n" +
+            "- Reducir el consumo de carne roja a una vez por semana puede disminuir tu huella de carbono en 0.5 toneladas al año."
+        );
+    } else if (transporte === "nunca" && carne) {
+        alert("- Reducir el consumo de carne roja a una vez por semana puede disminuir tu huella de carbono en 0.5 toneladas al año.");
+    } else if (transporte === "nunca" && !carne) {
+        alert("No necesitas consejos, gracias por ayudar a reducir el cambio climático.");
+    }
+
+    opciones();
+}
+
+iniciarAplicacion();
